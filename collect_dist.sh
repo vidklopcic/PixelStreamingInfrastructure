28514fa1311edd270a4544202ee7ae964e69ae55
vidklopcic <klopcic.vid@gmail.com>
2024-07-31 10:27:55 +0200

    collect artefacts

--- collect_dist.sh
+++ collect_dist.sh
@@ -3,6 +3,7 @@
 rm -rf LgmDist
 mkdir LgmDist LgmDist/Frontend LgmDist/Signalling
 cp -r Frontend/implementations/lgm_metahuman/dist/* LgmDist/Frontend/
-cp -r LGMWebServer/dist/* LgmDist/Signalling/
+cp -r LGMWebServer/* LgmDist/Signalling/
+rm -rf LgmDist/Signalling/node_modules LgmDist/Signalling/dist LgmDist/Signalling/.git
 tar -czvf LgmDist.tar.gz LgmDist
 rm -rf LgmDist
