967af70c5abf0ababaade86e24550f3d267d883a
vidklopcic <klopcic.vid@gmail.com>
2025-05-05 17:26:30 +0200

    added turn server

--- Frontend/implementations/lgm_metahuman/src/lgm/stores/LgmWebRTCStore.ts
+++ Frontend/implementations/lgm_metahuman/src/lgm/stores/LgmWebRTCStore.ts
@@ -151,6 +151,11 @@ export class LgmWebRTCStore {
             iceServers: [
                 { urls: 'stun:stun.l.google.com:19302' },
                 { urls: 'stun:stun1.l.google.com:19302' },
+                {
+                    urls: 'turn:openrelay.metered.ca:80',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject'
+                },
             ],
             iceCandidatePoolSize: 10
         });
